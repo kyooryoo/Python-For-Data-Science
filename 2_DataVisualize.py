e047d660ddbb7372e1057c81bf65f49523068dd5
Ling Jiang <kyooryoo@hotmail.com>
2017-10-19 17:36:44 -0400

    basic data visualiztion complete

--- 2_DataVisualize.py
+++ 2_DataVisualize.py
@@ -1,3 +1,7 @@
+# for installing the library of matplotlib:
+# http://matplotlib.org/users/installing.html#installing-an-official-release
+
+# this script is written and tested in windows 10 with python v3
 from matplotlib import pyplot as plt
 
 # use line chart to show the trend of some data
@@ -23,11 +27,13 @@ plt.xticks([i for i, _ in enumerate(movies)], movies)
 plt.show()
 
 # histogram
+# import Counter from collections lib
+from collections import Counter
 grades = [83,95,91,87,70,0,85,82,100,67,73,77,0]
 decile = lambda grade: grade // 10 * 10
 # Counter function does not work in Python v3.x
 histogram = Counter(decile(grade) for grade in grades)
-plt.bar([x - 4 for x  in histogram.keys()],histogram.values(),8)
+plt.bar([x for x  in histogram.keys()],histogram.values(),8)
 # x-axis from -5 to 105 and y-axis from 0 to 5
 plt.axis([-5, 105, 0, 5])
 # x-axis labels at 0, 10, ..., 100
@@ -35,4 +41,65 @@ plt.xticks([10 * i for i in range(11)])
 plt.xlabel("Decile")
 plt.ylabel("# of Students")
 plt.title("Distribution fo Exam Grades")
+plt.show()
+
+# pay attention to the start point of y-axis
+mentions = [500, 505]
+years = [2013, 2014]
+# in Unix replace following with: 
+# plt.bar([2012.6,2013.6],mentions,0.8)
+plt.bar([2013,2014],mentions,0.8)
+plt.xticks(years)
+plt.ylabel("# of times I heard people say 'data science'")
+# in Unix add: plt.ticklabel_format(useOffset=False)
+# y-axis not start from 0 can be misleading
+plt.axis([2012.5,2014.5,499,506])
+plt.title("Look at the 'HUGE' increase")
+plt.show()
+plt.bar([2013,2014],mentions,0.8)
+plt.xticks(years)
+plt.ylabel("# of times I heard people say 'data science'")
+# with more sensible axes, the difference is less impressive
+plt.axis([2012.5,2014.5,0,550])
+plt.title("Not So Huge Anymore")
+plt.show()
+
+# multiple line chart
+variance = [1,2,4,8,16,32,64,128,256]
+bias_squared = [256,128,64,32,16,8,4,2,1]
+total_error = [x + y for x, y in zip(variance, bias_squared)]
+xs = [i for i, _ in enumerate(variance)]
+# call plt.plot multiple times to draw series of lines
+plt.plot(xs, variance, 'g-', label='variance') # green solid line
+plt.plot(xs, bias_squared, 'r-.', label='bias^2') # red dot-dashed line
+plt.plot(xs, total_error, 'b:', label='total error') # blue dotted line
+plt.legend(loc=9)
+plt.xlabel("model complexity")
+plt.title("The Bias-Variance Tradeoff")
+plt.show()
+
+# use scatterplots to visualize two paired sets of data
+friends = [70,65,72,63,71,64,60,64,67]
+minutes = [175,170,205,120,220,130,105,145,190]
+labels = ['a','b','c','d','e','f','g','h','i']
+plt.scatter(friends,minutes)
+# label each point
+for label, friend_count, minute_count in zip(labels, friends, minutes):
+    plt.annotate(label, 
+            xy=(friend_count, minute_count), # put label with its point 
+            xytext=(5,-5), textcoords='offset points')
+plt.title("Daily Minutes vs. Number of Friends")
+plt.xlabel("# of friends")
+plt.ylabel("daily minutes spent on the site")
+plt.show()
+
+# specify the scale to show a less misleading picture
+test_1_grades = [90,90,85,97,80] # with less variance
+test_2_grades = [100,85,60,90,70] # with more variance
+# add or remove the setting below to find the difference
+# plt.axis("equal")
+plt.scatter(test_1_grades,test_2_grades)
+plt.title("Axes aren't comparable")
+plt.xlabel("test 1 grade")
+plt.ylabel("test 2 grade")
 plt.show()
