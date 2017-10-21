c9cde2d5e7a0a050ff78583057dd231b1e9a5151
Ling Jiang <kyooryoo@hotmail.com>
2017-10-21 08:47:21 -0400

    Linear Algebra functions added

---
+++ 3_LinearAlgebraFunctions.py
@@ -0,0 +1,72 @@
+# the functions for Vectors
+def vector_add(v, w):
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+def vector_substract(v, w):
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
+
+def vector_sum(vectors):
+    result = vectors[0]
+    for vector in vectors[1:]:
+        result = vector_add(result, vector)
+    return result
+# another way for sum vectors:
+def vector_sum_1(vectors):
+    return reduce(vector_add, vectors)
+# or
+vector_sum_2 = partial(reduce, vector_add)
+
+def scalar_multiply(c, v):
+    return [c * v_i for v_i in v]
+
+def vector_mean(vectors):
+    n = len(vectors)
+    return scalar_multiply(1/n, vector_sum(vectors))
+
+# doc product is the sum of products of 
+# the same positioned elements in vectors v and w
+def dot(v, w):
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+def sum_of_squares(v):
+    return dot(v, v)
+
+import math
+def magnitude(v):
+    return math.sqrt(sum_of_squares(v))
+
+def squared_distance(v, w):
+    return sum_of_squares(vector_subtract(v, w))
+
+def distance(v, w):
+    return math.sqrt(squared_distance(v, w))
+
+# as the equivalent
+def distance(v, w):
+    return magnitude(vector_subtract(v, w))
+
+
+# the functions fo matrics
+def shape(A):
+    num_rows = len(A)
+    num_cols = len(A[0]) if A else 0
+    return num_rows, num_cols
+
+def get_row(A, i):
+    return A[i]
+
+def get_col(A, j):
+    return [A_i[j] for A_i in A] # for each row A_i
+
+def make_matrix(num_rows, num_cols, entry_fn):
+    return [[entry_fn(i, j) for j in range(num_cols)] for i in range(num_rows)]
+
+# following is an example of entry_fn
+def is_diagonal(i, j):
+    return 1 if i == j else 0
+
+indentity_matrix = make_matrix(5, 5, is_diagonal)
+
+# learning resources of Linear Algebra
+# https://www.math.ucdavis.edu/~linear/
+# http://joshua.smcvt.edu/linearalgebra
