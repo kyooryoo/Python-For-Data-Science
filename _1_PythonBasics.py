e3687f096f068a1bc9c79012f9fea52549bd8e9b
Ling Jiang <kyooryoo@hotmail.com>
2017-12-22 22:38:40 -0500

    updated code to Python3

--- _1_PythonBasics.py
+++ _1_PythonBasics.py
@@ -1,24 +1,20 @@
 # list comprehensions
 even_numbers = [x for x in range(5) if x % 2 == 0]
-print(even_numbers)
+print("a list of even numbers: {}".format(even_numbers))
 squares = [x * x for x in range(5)]
-print(squares)
+print("square the numbers from 0 to 4: {}".format(squares))
 even_squares = [x * x for x in even_numbers]
-print(even_squares)
+print("square the even numbers: {}".format(even_squares))
 square_dict = {x : x * x for x in range(5)}
-print(square_dict)
+print("the squred number dictionary: {}".format(square_dict))
 square_set = {x * x for x in range(-5, 5)}
-print(square_set)
+print("a set can only contain unique objects: {}".format(square_set))
 zeros = [ 0 for _ in even_numbers]
-print(zeros)
-pairs = [(x, y)
-         for x in range(3)
-         for y in range(3)]
-print(pairs)
-increasing_pairs = [(x, y)
-                    for x in range(3)
-                    for y in range(x + 1, 3)]
-print(increasing_pairs)
+print("use a list as index: {}".format(zeros))
+pairs = [(x, y) for x in range(2) for y in range(3)]
+print("a list of pared numbers: {}".format(pairs))
+increasing_pairs = [(x, y) for x in range(5) for y in range(x + 1, 5)]
+print("a list of increased numbers: {}".format(increasing_pairs))
 
 # generate number by need
 def lazy_range(n):
@@ -27,48 +23,46 @@ def lazy_range(n):
         yield i
         i += 1
 lazy_evens_below_20 = [i for i in lazy_range(20) if i % 2 == 0]
-print(lazy_evens_below_20)
+print("the even number below 20: {}".format(lazy_evens_below_20))
 
 # generate random number
 import random
 four_uniform_randoms = [random.random() for _ in range(4)]
-print(four_uniform_randoms)
+print("random 4 uniform numbers: {}".format(four_uniform_randoms))
 # specify seed to get reproducible results
 random.seed(10)
-print(random.random())
+print("a reproducible random number: {}".format(random.random()))
 random.seed(10)
-print(random.random())
+print("a reproduced random number: {}".format(random.random()))
 # random selection from range
-random.seed(10)
-print(random.randrange(10))
-print(random.randrange(5, 10))
+print("random select from range 0 to 9: {}".format(random.randrange(10)))
+print("random select from range 7 to 9: {}".format(random.randrange(8, 10)))
 # random reorder the elements
 up_to_ten = [x for x in range(10)]
 random.shuffle(up_to_ten)
-print(up_to_ten)
+print("random order of 0 to 9: {}".format(up_to_ten))
 # random select element
 my_favorite_fruit = random.choice(["apple","banana","orange"])
-print(my_favorite_fruit)
+print("random select elements: {}".format(my_favorite_fruit))
 # random select element without duplicate
 lottery_numbers = [x for x in range(60)]
 winning_numbers = random.sample(lottery_numbers, 6)
-print(winning_numbers)
+print("random select unduplicated elements: {}".format(winning_numbers))
 # random select element with duplicate
-ten_with_replacement = [random.choice(range(10))
-                         for _ in range(10)]
-print(ten_with_replacement)
+ten_with_replacement = [random.choice(range(10)) for _ in range(10)]
+print("random select duplicatible elements: {}".format(ten_with_replacement))
 
 # regular expression
 import re
 # use "not" to convert result to boolean value
-result = not re.match("a", "cat")
-print("'cat' starts with 'a': " + str(not result))
+result = not re.match("a", "cat") # returned value is None by default
+print("'cat' starts with 'a': {}".format(not result)) # convert to boolean
 result = not re.search("a", "cat")
-print("'cat' has 'a': " + str(not result))
+print("'cat' has 'a': {}".format(not result))
 result = re.split("[yo]","python")
-print("split python by 'y' and 'o' get: " + str(result))
+print("split python by 'y' and 'o' get: {}".format(result))
 result = re.sub("[0-9]","-","R2D2")
-print("replace digit in R2D2 with dash: " + result)
+print("replace digit in R2D2 with dash: {}".format(result))
 
 # object oriented programming
 class Set:
@@ -76,9 +70,10 @@ class Set:
   self.dict = {}
   if values is not None:
    for value in values:
-    self.add(value)
+    if value not in self.dict:
+     self.add(value)
  def __repr__(self):
-  return "Set: " + str(self.dict.keys())
+  return str(self.dict.keys())
  def add(self, value):
   self.dict[value] = True
  def contains(self, value):
@@ -87,128 +82,140 @@ class Set:
   del self.dict[value]
 
 s = Set([1,2,3])
+print("the initial set: {}".format(s))
 s.add(4)
-print s.contains(4)
+print("after 4 added: {}".format(s))
+print("the set contains 4: {}".format(s.contains(4)))
 s.remove(3)
-print s.contains(3)
+print("3 is removed: {}".format(s))
+print("the set contains 3: {}".format(s.contains(3)))
+s.add(4)
+print("try adding one more 4: {}".format(s))
 
-print "# functional tools"
-# the function of exp takes two arguments
+import functools as ft
+# functional tools
 def exp(base, power):
  return base ** power
 # without functional tools
 def two_to_the(power):
  return exp(2, power)
-# with functional tools
-# provides partial arguments for exp
-# which fulfills the first argument by default
-from functools import partial
-two_to_the = partial(exp, 2)
-print two_to_the(3)
+print("the specifically defined two_to_the(3): {}".format(two_to_the(3)))
+# functools provides partial arguments for exp
+# fulfills the first argument by default
+new_two_to_the = ft.partial(exp, 2)
+print("the newly defined new_two_to_the(3): {}".format(new_two_to_the(3)))
 # with specified argument name
 # second argument could be fulfilled in advance
-square_of = partial(exp, power=2)
-print square_of(3)
+square_of = ft.partial(exp, power=2)
+print("also easily defined squre_of(3): {}".format(square_of(3)))
+
 # there are different ways to achieve the same purpose
 def double(x):
  return 2 * x
 xs = [1,2,3,4]
+print("the original list is: {}".format(xs))
 # use for
 twice_xs = [double(x) for x in xs]
-print twice_xs
-# use map
-twice_xs = map(double, xs)
-print twice_xs
-# use map with partial
-list_doubler = partial(map, double)
-twice_xs = list_doubler(xs)
-print twice_xs
+print("twice the list with for loop: {}".format(twice_xs))
+# use map, should be wrapped with list() to display content in Python3
+twice_xs = list(map(double, xs))
+print("use map to apply func to var: {}".format(twice_xs))
+# use partial with map to construct a new function
+list_doubler = ft.partial(map, double)
+twice_xs = list(list_doubler(xs))
+print("use partial with map: {}".format(twice_xs))
 # use map with multiple arguments
 def multiply(x, y): return x * y
-products = map(multiply, [1,2],[4,5])
-print products
+products = list(map(multiply, [1,2],[4,5]))
+print("the products of [1,2] and [4,5]: {}".format(products))
+
 # filter works as a list comprehension if
+# filter need a function to return the boolean
 def is_even(x):
  return x % 2 == 0
 x_evens = [x for x in xs if is_even(x)]
-print x_evens
-x_evens = filter(is_even, xs)
-print x_evens
+print("for loop based filter: {}".format(x_evens))
+x_evens = list(filter(is_even, xs))
+print("filter works the same as map: {}".format(x_evens))
 # combined with partial
-list_evener = partial(filter, is_even)
-x_evens = list_evener(xs)
-print x_evens
+list_evener = ft.partial(filter, is_even)
+x_evens = list(list_evener(xs))
+print("filter could also work with partial: {}".format(x_evens))
+
 # reduce works on the elements iteratively
-x_product = reduce(multiply, xs)
-print x_product
-list_product = partial(reduce, multiply)
-x_product = list_product(xs)
-print x_product
+x_product = ft.reduce(multiply, xs)
+print("use reduce to iteratively multiply numbers in list: {}"
+      .format(x_product))
+list_product = ft.partial(ft.reduce, multiply)
+print("combine partial with reduce to create function: {}"
+      .format(list_product(xs)))
+
 # use index and elements in non-pythonic way
 for index in range(len(xs)):
  element = xs[index]
- print str(index) + " : " + str(element)
-# or
+ print("non-pythonic way: {} -- {}".format(str(index),str(element)))
+# python way does not use index to specify list members
 index = 0
 for element in xs:
- print str(index) + " : " + str(element)
+ print("pythonic way: {} -- {}".format(str(index),str(element)))
  index += 1
 # use enumerate as the pythonic solution
 # replace element with "_" if only need index
 for index, element in enumerate(xs):
- print str(index) + " : " + str(element)
+ print("enumerate way: {} -- {}".format(str(index),str(element)))
 
-print "# use zip to pack or unpack lists"
+# use zip to pack or unpack lists
 list1 = ['a','b','c']
 list2 = [1,2,3]
-zipped = zip(list1,list2)
-print zipped
+zipped = list(zip(list1,list2))
+print("zipped list of ['a','b','c'] and [1,2,3]: {}".format(zipped))
 # use "*" to pass elements of zipped
 # as individual argument to zip for unzipping
 letters, numbers = zip(*zipped)
-print letters
-print numbers
+print("unzipped letters: {}".format(letters))
+print("unzipped numbers: {}".format(numbers))
 # zip the zipped object to unzip it
-unzipped = zip(*zipped)
-print unzipped 
+unzipped = list(zip(*zipped))
+print("unzipped together: {}".format(unzipped)) 
 # "*" could be very convenient
 def add(a,b): return a + b
-print add(1,2)
+print("the result of add(1,2): {}".format(add(1,2)))
 # add([1,2]) will be an error
-print add(*[1,2]) # this works
+print("the result of add(*[1,2]): {}".format(add(*[1,2]))) # this works
 
-# sometimes a function takes 
-# another function as input
-# and returns a function
-def doubler(f):
+# a function can take another function as input and returns a function
+def double(f):
  def g(x):
   return 2 * f(x)
  return g
 # it works for single argument
-def f1(x):
+def addone(x):
  return x + 1
-g = doubler(f1)
-print g(3) # (3+1)*2=8
-print g(-1) # (-1+1)*2=0
-#it does not work below
-def f2(x, y):
- return x + y
-g = doubler(f2)
-# print g(1,2) will generate error
+addonethendouble = double(addone)
+print("double func take addone func as parameter, input 3: {}"
+      .format(addonethendouble(3))) # (3+1)*2=8
+print("same as above, input 1: {}".format(addonethendouble(-1))) # (-1+1)*2=0
+# it does not work below
+def add(x, y):
+ return (x + y)
+addthendouble = double(add)
+# print addthendouble(1,2) will generate error
+
 # *args and **kwargs take arbitrary arguments
 # args is a tuple of unnamed arguments
 # kwargs is a dict of named arguments
 def magic(*args, **kwargs):
- print "unnamed args: ", args
- print "keyword args: ", kwargs
+ print("unnamed args: ", args)
+ print("keyword args: ", kwargs)
 magic(1,2,key1="word1",key2="word2")
 # so this works
-def doubler_correct(f):
+def double_correct(f):
  def g(*args, **kwargs):
   return 2 * f(*args, **kwargs)
  return g
-g = doubler_correct(f2)
-print g(1,2) # (1+2)*2=6
+addanddouble = double_correct(add)
+print("double func take add func as parameter, input 1 and 2 : {}"
+      .format(addanddouble(1,2))) # (1+2)*2=6
 
 # For further exploration
 # official tutorial at https://docs.python.org/3/tutorial
@@ -216,4 +223,3 @@ print g(1,2) # (1+2)*2=6
 # and also http://ipython.org/documentation.html
 # and http://ipython.org/presentation.html
 # A good reference book of Python for Data Analysis by William McKinney
-
